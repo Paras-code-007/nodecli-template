4082b79ee8608948948ffa3c715efbddcaa42231
Paras-code-007 <paras.arora0025@gmail.com>
2020-11-08 18:37:12 +0530

    work around to pace debug at end

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 const init= require('./utils/init')
 const generate= require('./utils/generate')
 const cli= require('./utils/cli')
+const debug= require('./utils/debug')
 
 ;(async ()=>{
+
     init(cli.flags)
     cli.input.includes('help') && cli.showHelp()
-    const vars=await generate(cli)  //returns a pending promis therfore awaited it 
-    // const userinputs= {...vars}
+    const vars=await generate()  //returns a pending promis therfore awaited it 
+    const userinputs= {...vars}
+    
+    process.on('exit',function (exitStatus) {
+        console.log('exit code:', exitStatus)
+        debug(cli.flags.debug, cli.flags, cli.input, userinputs)
+    })
+
 })()
