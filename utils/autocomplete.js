d00c317c707b2bd2ff3518b2c8bbab4f39bf639a
Paras-code-007 <paras.arora0025@gmail.com>
2020-11-09 08:42:43 +0530

    improved

--- utils/autocomplete.js
+++ utils/autocomplete.js
@@ -3,6 +3,8 @@ const to= require('await-to-js').default
 const handleError= require('cli-display-error')
 const choices= require('./licensedata')
 const {magenta}= require('chalk')
+const {Store}= require('data-store')
+const path= require('path')
 
 module.exports= async ({name, message, hint})=>{
     const [err,response]= await to(new AutoComplete({
@@ -11,9 +13,13 @@ module.exports= async ({name, message, hint})=>{
         limit: 5,
         choices,
         hint,
+        // history: {
+        //     autosave: true,
+        //     store: new Store({path: path.join(__dirname, `./../.history/${name}.json`)})  
+        // } //!not working
         // validate(value,state){
         //     return !value? "Please add value" : true
-        // }
+        // } //!not working
     })
     .on('cancel', ()=> {
         console.log(magenta.bold('❯ Cancelled'))
